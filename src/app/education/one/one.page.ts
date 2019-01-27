1fe92e3c230affaf1fcca3031afa6bb8767828b5
DingLin <tim920323@gmail.com>
2019-01-27 06:05:31 -0500

    backup

--- src/app/education/one/one.page.ts
+++ src/app/education/one/one.page.ts
@@ -1,19 +1,14 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-one',
   templateUrl: './one.page.html',
   styleUrls: ['./one.page.scss'],
 })
-export class One {
-  name: String;
-  age: number;
-  constructor(public navCtrl: NavController, public navParam: NavParams) {
-    this.name = this.navParam.get('name');
-    this.age = this.navParam.get('age');
+export class OnePage {
   }
   // back() {
   //   this.navCtrl.pop();
-}
+
 
